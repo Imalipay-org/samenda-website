e0dc228c6d1138531e5848d3a0f5d7afdd53f927
freeman-imali <freeman@imalipay.com>
2024-03-19 23:44:10 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App-main">
+      <header className="App-header">
+      </header>
+
+      <body className="App-body">
+      </body>
+    </div>
+  );
+}
+
+export default App;
